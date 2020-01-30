95d570a533581ab511e490f1317abb17fc08c7bb
chasingw <cchasingw@gmail.com>
2020-01-30 13:35:44 +0800

    remove prefix

--- v2ray.sh
+++ v2ray.sh
@@ -132,7 +132,7 @@ create_vmess_URL_config() {
 		cat >/etc/v2ray/vmess_qr.json <<-EOF
 		{
 			"v": "2",
-			"ps": "maxwell_${domain}",
+			"ps": "${domain}",
 			"add": "${domain}",
 			"port": "443",
 			"id": "${v2ray_id}",
@@ -149,7 +149,7 @@ create_vmess_URL_config() {
 		cat >/etc/v2ray/vmess_qr.json <<-EOF
 		{
 			"v": "2",
-			"ps": "maxwell_${ip}",
+			"ps": "${ip}",
 			"add": "${ip}",
 			"port": "${v2ray_port}",
 			"id": "${v2ray_id}",
